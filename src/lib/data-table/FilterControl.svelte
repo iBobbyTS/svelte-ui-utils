275d865ecca4c2f86a0349bfaa38f5d1fdeabb76
iBobby <ibobbyts@gmail.com>
2026-08-22 16:40:09 -0600

    fix(table): 仅扩展分隔线两侧间距
    恢复普通筛选项的原有间距，仅为带 divider 的分组末项增加左右留白。

--- src/lib/data-table/FilterControl.svelte
+++ src/lib/data-table/FilterControl.svelte
@@ -41,7 +41,7 @@
     {/each}
   </div>
 {:else if control.type === 'checkbox'}
-  <div class="suu-filter-table__options" class:suu-filter-table__options--grouped={Boolean(control.optionGroups?.length)}>
+  <div class="suu-filter-table__options">
     {#if control.optionGroups?.length}
       {#each control.optionGroups as group, groupIndex}
         {#each group.options as option, optionIndex}
